4950806b28da4185a622d1b05f4a9ccae79d153a
Vince Howard <vincenguyenhoward@gmail.com>
2019-01-04 19:29:07 -0700

    added middleware for user data fetching on each request and added permissions

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,17 @@ server.express.use((req, res, next) => {
 	next();
 });
 
+// 2. Create middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+	// if they aren't logged in, skip this
+	if (!req.userId) return next();
+
+	const user = await db.query.user({ where: { id: req.userId } }, `{ id, permissions, email, name }`);
+	console.log(user);
+	req.user = user;
+	next();
+});
+
 server.start(
 	{
 		cors: {
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils.js');
 
 const Query = {
 	// items: forwardTo('db') // quick way with no custom logic
@@ -15,6 +16,16 @@ const Query = {
 			},
 			info
 		);
+	},
+	async users(parent, args, ctx, info) {
+		// 1. Check if they are logged in
+		if (!ctx.request.userId) {
+			throw new Error('You must be logged in');
+		}
+		// 2. Check if the user has the permissions to query all the uesrs
+		hasPermission(ctx.request.user, [ 'ADMIN', 'PERMISSIONUPDATE' ]);
+		// 3. If they do, query all the users
+		return ctx.db.query.users({}, info);
 	}
 	// item: forwardTo('db'),
 	// async items(parent, args, ctx, info) {
